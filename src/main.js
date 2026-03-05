c68d619d93fbd120bb10080a633c3a63b48aea5f
BelSerhij <belkasergij@gmail.com>
2026-03-05 14:41:04 +0200

    save

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import { createModalMarkup } from './animal_detail.js'; 
+
+
+
+async function fetchPetById() {
+  try {
+    const response = await fetch(`https://paw-hut.b.goit.study/api/animals`);
+    if (!response.ok) throw new Error("Помилка запиту");
+    
+    const data = await response.json();
+    
+    console.log("Отримана тваринка:", data[0]); 
+    return data[0]; 
+  } catch (error) {
+    console.error("Помилка:", error);
+  }
+}
